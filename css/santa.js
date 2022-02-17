b81887f9dddc9fbc268bf40b38bc69de3051e42a
eshwar-war <85940841+eshwar-war@users.noreply.github.com>
2022-02-17 20:53:04 +0530

    Add files via upload

---
+++ css/santa.js
@@ -0,0 +1,34 @@
+/*function sub(){
+    var subrat=950
+    var plus=50
+    var result=subrat-plus
+    console.log(result)
+
+
+sub()*/
+
+/*function sum(x,y,z,){
+    var eshwar =x
+    var indhu =y
+    var daddy = z
+  
+    var result=eshwar+indhu+daddy
+    console.log(result)
+}
+sum(100,200,300)
+sum(20,40,50)*/
+
+function fn(){/*
+    var myobject={
+        name:"eshwar",
+        place:"Rayadurga",
+        workPlace:"Banglore"
+
+    }
+    console.log(myobject)
+}
+fn()*/
+
+document.getElementById("btn").onclick=function () {
+    alert("Hello world")
+}
